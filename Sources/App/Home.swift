c05b1beef1cd81d33843abe15ae88d20c5764f57
mrc <marco.capano@pushapp.me>
2019-01-10 15:24:32 +0100

    fixed link and added spacing

--- Sources/App/Home.swift
+++ Sources/App/Home.swift
@@ -20,14 +20,15 @@ extension HtmlProvider {
         
         let content = div([`class`("page-content")], [
             p([
-                "Hi, it's Marco! I build iOS apps, contribute to open source and study Computer Engineering. I became an iOS Developer in 2016 studying at the Apple Developer Academy in Italy, then started working while starting my Computer Engineering program. I love basketball, books, TV series and tech. I like contributing to open source ", a([`class`("link"), openSource], ["(see here)"]), " and recently started contributing ", a([`class`("link"), writing],["to the italian version of NSHipster.com"]), ". You can find some of the apps I published on the App Store ",
+                "Hi, it's Marco! I build iOS apps, contribute to open source and study Computer Engineering. I became an iOS Developer in 2016 studying at the Apple Developer Academy in Italy, then started working while starting my Computer Engineering program."]),
+            p(["I love basketball, books, TV series and tech. I like contributing to open source ", a([`class`("link"), openSource], ["(see here)"]), " and recently started contributing ", a([`class`("link"), writing],["to the italian version of NSHipster.com"]), ". You can find some of the apps I published on the App Store ",
                 a([`class`("link"), apps], ["here"]), "."
             ]),
             
             h2([style("color: rgb(51,51,51)")], ["Get in touch!"]),
             
             ul([
-                li(["Email: ", a([`class`("link"), mail], ["Email"])]),
+                li(["Email: ", a([`class`("link"), mail], ["marco24capano@icloud.com"])]),
                 li(["GitHub: ", a([`class`("link"), github], ["@marcocapano"])]),
                 li(["Twitter: ", a([`class`("link"), twitter], ["@marcocapano1"])]),
                 li(["Linkedin: ", a([`class`("link"), linkedin], ["@marcocapano"])])
