8513c1a3defdae7f781373a5af32220979dc5507
mrc <marco.capano@pushapp.me>
2019-01-22 11:10:36 +0100

    updating link

--- Sources/App/Articles.swift
+++ Sources/App/Articles.swift
@@ -11,7 +11,7 @@ import Html
 extension HtmlProvider {
     static func articles() -> Node {
         let articles: [Item] = [
-            Item(title: "Calling a function n times with the result of each call", url: "https://marcocapano.vapor.cloud/articles/call-n-times", tag: "SWIFT", description: "Today I faced this question from a tweet by Sash Zats: how does one call a function n times, using as a parameter for each call the result of the previous call? The answer, as usually happens, is in the Swift Standard Library.")
+            Item(title: "Calling a function n times with the result of each call", url: "https://marcocapano.vapor.cloud/articles/Calling-a-function-n-times-with-the-result-of-each-call", tag: "SWIFT", description: "Today I faced this question from a tweet by Sash Zats: how does one call a function n times, using as a parameter for each call the result of the previous call? The answer, as usually happens, is in the Swift Standard Library.")
         ]
         
         
